0bab030d44c35284687784ce486dc9f3b1982bd8
David Lambeth <david.lambeth@outlook.com>
2020-12-14 21:37:05 +0000

    Weird Gloop Update

--- src/main/java/runescape/UrlConstants.java
+++ src/main/java/runescape/UrlConstants.java
@@ -8,6 +8,7 @@ public class UrlConstants {
     public static String DETAIL_SUFFIX = "catalogue/detail.json?item={ITEM_ID}";
     public static String GRAPH_SUFFIX = "graph/{ITEM_ID}.json";
     public static String WIKI_PREFIX = "https://runescape.wiki/w/Module:Exchange/{ITEM_NAME}/Data";
+    public static String WEIRD_GLOOP_PREFIX = "https://api.weirdgloop.org/exchange/history/rs/sample?compress=true&id={ITEM_ID}";
 
     public static String ITEM_ID_REPLACE = "{ITEM_ID}";
     public static String CATEGORY_ID_REPLACE = "{CATEGORY_ID}";
